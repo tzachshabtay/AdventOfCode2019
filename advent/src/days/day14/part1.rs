2df90c707cb09dc42f2b07f882fb04cb7fea936b
Tzach Shabtay <tshabtay@clearstreet.io>
2019-12-22 21:57:59 -0500

    Day 14- part 2

--- advent/src/days/day14/part1.rs
+++ advent/src/days/day14/part1.rs
@@ -1,14 +1,15 @@
 use crate::utils;
 use std::collections::HashMap;
+use std::cmp;
 
 pub fn run() {
     let reactions = parse();
-    let mut solver = Solver::new(reactions);
-    let result = solver.solve(Chemical::new("1 FUEL"));
+    let mut solver = Solver::new(HashMap::new());
+    let result = solver.solve(&Chemical::new("FUEL".to_string(), 1), &reactions);
     println!("{}", result);
 }
 
-fn parse() -> HashMap::<String, Reaction> {
+pub fn parse() -> HashMap::<String, Reaction> {
     let lines = utils::lines_from_file("./src/days/day14/input.txt");
     let mut reactions = HashMap::new();
     for line in lines {
@@ -18,66 +19,81 @@ fn parse() -> HashMap::<String, Reaction> {
     reactions
 }
 
-struct Solver {
-    reactions: HashMap::<String, Reaction>,
-    inventory: HashMap::<String, usize>,
+pub struct Solver {
+    pub inventory: HashMap::<String, usize>,
 }
 
 const ORE: &'static str = "ORE";
 
 impl Solver{
-    fn new(reactions: HashMap::<String, Reaction>) -> Solver {
-        Solver{reactions, inventory: HashMap::new()}
+    pub fn new(inventory: HashMap::<String, usize>) -> Solver {
+        Solver{inventory}
     }
 
-    fn solve(&mut self, needed: Chemical) -> usize {
+    pub fn solve(&mut self, needed: &Chemical, reactions: &HashMap::<String, Reaction>) -> usize {
         if needed.name == ORE {
             return needed.qty;
         }
-        let mut remaining = 0;
+        let (mut in_inventory, mut needed_qty) = (0, needed.qty);
         if self.inventory.contains_key(&needed.name) {
-            remaining = self.inventory[&needed.name];
+            in_inventory = self.inventory[&needed.name];
         }
-        if remaining > needed.qty {
-            self.inventory.insert(needed.name.to_string(), remaining - needed.qty);
-            println!("{} {}- had it!", &needed.name, needed.qty);
+        if in_inventory > 0 {
+            let used = cmp::min(in_inventory, needed_qty);
+            self.inventory.insert(needed.name.to_string(), in_inventory - used);
+            needed_qty -= used;
+        }
+        if needed_qty == 0 {
             return 0;
         }
-        if remaining > 0 {
-            self.inventory.remove(&needed.name);
-        }
-        let reaction = self.reactions[&needed.name].clone();
+        let reaction = &reactions[&needed.name];
         let mut sum = 0;
-        let mut needed_qty = (needed.qty - remaining) as isize;
-        while needed_qty > 0 {
+        let mut factor = 1;
+        let out_qty = reaction.output.qty;
+        if needed_qty > out_qty {
+            if needed_qty % out_qty == 0 {
+                factor = needed_qty / out_qty;
+            } else {
+                factor = (needed_qty / out_qty) + 1;
+            }
+        }
+        self.inventory.insert(needed.name.to_string(), (out_qty * factor - needed_qty) as usize);
+        for input in &reaction.inputs {
+            sum += self.solve(&Chemical::new(input.name.to_string(), input.qty * factor), reactions);
+        }
+        /*while needed_qty > 0 {
             for input in &reaction.inputs {
-                sum += self.solve(input.clone());
+                sum += self.solve(input, reactions);
             }
             needed_qty -= reaction.output.qty as isize;
         }
         if needed_qty < 0 {
             self.inventory.insert(needed.name.to_string(), -needed_qty as usize);
-        }
-        println!("{} {}- {}! (reused {})", &needed.name, needed.qty, sum, remaining);
+        }*/
+        //println!("{} {}- {}! (reused {})", &needed.name, needed.qty, sum, remaining);
         sum
     }
 }
 
 #[derive(Clone, Debug)]
-struct Chemical {
+pub struct Chemical {
     name: String,
     qty: usize,
 }
 
 impl Chemical {
-    fn new(line: &str) -> Chemical {
+    pub fn parse(line: &str) -> Chemical {
         let tokens: Vec<&str> = line.trim().split(" ").collect();
         Chemical{name: tokens[1].trim().to_string(), qty: tokens[0].trim().parse::<usize>().unwrap()}
+    }
+
+    pub fn new(name: String, qty: usize) -> Chemical {
+        Chemical{name, qty}
     }
 }
 
 #[derive(Clone, Debug)]
-struct Reaction {
+pub struct Reaction {
     inputs: Vec<Chemical>,
     output: Chemical,
 }
@@ -86,6 +102,6 @@ impl Reaction {
     fn new(line: &str) -> Reaction {
         let tokens : Vec<&str> = line.split("=>").collect();
         let (inputs, output) = (tokens[0].trim(), tokens[1].trim());
-        Reaction{inputs: inputs.split(",").map(|i| Chemical::new(i)).collect(), output: Chemical::new(output)}
+        Reaction{inputs: inputs.split(",").map(|i| Chemical::parse(i)).collect(), output: Chemical::parse(output)}
     }
 }
--- advent/src/days/day14/part2.rs
+++ advent/src/days/day14/part2.rs
@@ -1,1 +1,30 @@
-pub fn run(){}
+use crate::days::day14::part1::{Solver, parse, Chemical};
+use std::collections::{HashMap};
+
+pub fn run(){
+    let reactions = parse();
+    let target = 1000000000000 as usize;
+    let (mut min, mut max) = (0, target);
+    while min < max {
+        let mid = (min + max) / 2;
+        let mut solver = Solver::new(HashMap::new());
+        let ore_per_fuel = solver.solve(&Chemical::new("FUEL".to_string(), mid), &reactions);
+        if ore_per_fuel == target {
+            min = mid;
+            max = mid;
+        } else if ore_per_fuel < target {
+            min = mid + 1;
+        } else {
+            max = mid - 1;
+        }
+    }
+
+    let mut solver = Solver::new(HashMap::new());
+    let ore_per_fuel = solver.solve(&Chemical::new("FUEL".to_string(), min), &reactions);
+    if ore_per_fuel < target {
+        println!("{}", min);
+    } else {
+        println!("{}", min - 1);
+    }
+
+}
